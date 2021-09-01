8c3e5307ed5ad3a3fae7e231c0d44475195ebe68
Connor Hasting <guardianx515@gmail.com>
2021-08-31 21:58:00 -0500

    Update code styling

--- src/main/kotlin/BinarySquareSubdivision.kt
+++ src/main/kotlin/BinarySquareSubdivision.kt
@@ -3,97 +3,97 @@ import processing.core.PGraphics
 
 class BinarySquareSubdivision : PApplet() {
 
-    val maxScreenSize = 880
+  val maxScreenSize = 880
 
-    lateinit var polygon: PGraphics
+  lateinit var polygon: PGraphics
 
-    override fun setup() {
-        polygon = createGraphics(width, height, P2D)
-        background(10)
-        generatePoints(
-            width / 2F,
-            height / 2F
-        ).also {
-            polygon.beginDraw()
-            drawPoints(it)
-            polygon.endDraw()
-        }
+  override fun setup() {
+    polygon = createGraphics(width, height, P2D)
+    background(10)
+    generatePoints(
+      width / 2F,
+      height / 2F
+    ).also {
+      polygon.beginDraw()
+      drawPoints(it)
+      polygon.endDraw()
+    }
+  }
+
+  override fun settings() {
+    size(maxScreenSize, maxScreenSize, P2D)
+  }
+
+  override fun draw() {
+    image(polygon, 0F, 0F)
+  }
+
+  fun drawPoints(coordinates: List<Coordinate>) {
+
+    println(coordinates.size)
+    // polygon.noFill()
+    coordinates.forEach {
+      println("${it.x} ${it.y}")
+      polygon.stroke(240)
+      polygon.strokeWeight(10F)
+      polygon.point(it.x, it.y)
+    }
+  }
+
+  fun generatePoints(x: Float, y: Float): List<Coordinate> {
+    var currentX = x
+    var currentY = y
+    val coordinates = mutableListOf<Coordinate>()
+
+    while (isValidPoint(currentX, currentY)) {
+      coordinates.add(Coordinate(currentX, currentY))
+      currentX /= 2f
+      currentY /= 2f
     }
 
-    override fun settings() {
-        size(maxScreenSize, maxScreenSize, P2D)
+    currentX = x
+    currentY = y
+
+    while (isValidPoint(currentX, currentY)) {
+      coordinates.add(Coordinate(currentX, currentY))
+      currentX += (abs(width - currentX) / 2F)
+      currentY += (abs(height - currentY) / 2F)
     }
 
-    override fun draw() {
-        image(polygon, 0F, 0F)
+    currentX = x
+    currentY = y
+
+    while (isValidPoint(currentX, currentY)) {
+      coordinates.add(Coordinate(currentX, currentY))
+      currentX /= 2f
+      currentY += (abs(height - currentY) / 2F)
     }
 
-    fun drawPoints(coordinates: List<Coordinate>) {
+    currentX = x
+    currentY = y
 
-        println(coordinates.size)
-        // polygon.noFill()
-        coordinates.forEach {
-            println("${it.x} ${it.y}")
-            polygon.stroke(240)
-            polygon.strokeWeight(10F)
-            polygon.point(it.x, it.y)
-        }
+    while (isValidPoint(currentX, currentY)) {
+      coordinates.add(Coordinate(currentX, currentY))
+      currentX += (abs(width - currentX) / 2F)
+      currentY /= 2f
     }
 
-    fun generatePoints(x: Float, y: Float): List<Coordinate> {
-        var currentX = x
-        var currentY = y
-        val coordinates = mutableListOf<Coordinate>()
+    return coordinates
+  }
 
-        while (isValidPoint(currentX, currentY)) {
-            coordinates.add(Coordinate(currentX, currentY))
-            currentX /= 2f
-            currentY /= 2f
-        }
-
-        currentX = x
-        currentY = y
-
-        while (isValidPoint(currentX, currentY)) {
-            coordinates.add(Coordinate(currentX, currentY))
-            currentX += (abs(width - currentX) / 2F)
-            currentY += (abs(height - currentY) / 2F)
-        }
-
-        currentX = x
-        currentY = y
-
-        while (isValidPoint(currentX, currentY)) {
-            coordinates.add(Coordinate(currentX, currentY))
-            currentX /= 2f
-            currentY += (abs(height - currentY) / 2F)
-        }
-
-        currentX = x
-        currentY = y
-
-        while (isValidPoint(currentX, currentY)) {
-            coordinates.add(Coordinate(currentX, currentY))
-            currentX += (abs(width - currentX) / 2F)
-            currentY /= 2f
-        }
-
-        return coordinates
+  private fun isValidPoint(x: Float, y: Float): Boolean {
+    if (x > (width - 10) || y > (height - 10) || x < 10F || y < 10F) {
+      return false
     }
-
-    private fun isValidPoint(x: Float, y: Float): Boolean {
-        if (x > (width - 10) || y > (height - 10) || x < 10F || y < 10F) {
-            return false
-        }
-        return true
-    }
+    return true
+  }
 }
 
 data class Coordinate(
-    val x: Float,
-    val y: Float
+  val x: Float,
+  val y: Float
 )
 
 fun main() {
-    PApplet.main("BinarySquareSubdivision")
+  PApplet.main("BinarySquareSubdivision")
 }
--- src/main/kotlin/CubeGrid.kt
+++ src/main/kotlin/CubeGrid.kt
@@ -5,58 +5,58 @@ import processing.core.PGraphics
 
 class CubeGrid : PApplet() {
 
-    val maxScreenSize = 880
-    lateinit var cube: PGraphics
+  val maxScreenSize = 880
+  lateinit var cube: PGraphics
 
-    val numCubesInRow = 11
-    val numCubes = numCubesInRow * numCubesInRow
+  val numCubesInRow = 11
+  val numCubes = numCubesInRow * numCubesInRow
 
-    val cubeSize = (maxScreenSize / 2) / numCubesInRow.toFloat()
+  val cubeSize = (maxScreenSize / 2) / numCubesInRow.toFloat()
 
-    override fun setup() {
-        cube = createGraphics(width, height, P3D)
-    }
+  override fun setup() {
+    cube = createGraphics(width, height, P3D)
+  }
 
-    override fun settings() {
-        size(maxScreenSize, maxScreenSize, P3D)
-    }
+  override fun settings() {
+    size(maxScreenSize, maxScreenSize, P3D)
+  }
 
-    override fun draw() {
-        background(10)
+  override fun draw() {
+    background(10)
 
-        runBlocking {
-            (0 until numCubes).forEach { index ->
-                launch {
-                    drawCube(index)
-                    image(cube, 0F, 0F)
-                }
-            }
+    runBlocking {
+      (0 until numCubes).forEach { index ->
+        launch {
+          drawCube(index)
+          image(cube, 0F, 0F)
         }
+      }
     }
+  }
 
-    fun drawCube(index: Int) {
-        cube.beginDraw()
-        cube.clear()
-        // cube.lights()
-        cube.stroke(230)
-        cube.strokeWeight(3F)
-        cube.noFill()
-        val row = index / numCubesInRow
-        val column = index % numCubesInRow
-        val widthIncrement = width / numCubesInRow.toFloat()
-        val heightIncrement = height / numCubesInRow.toFloat()
-        cube.translate(column * widthIncrement + cubeSize, row * heightIncrement + cubeSize)
-        val maxRotation = PI * 2
-        val xRotation = (maxRotation / numCubesInRow) * (row - numCubesInRow / 2) - PI
-        val yRotation = (maxRotation / numCubesInRow) * (column - numCubesInRow / 2) - PI
-        cube.rotateX((frameCount / 20.0F) + xRotation)
-        //cube.rotateY((frameCount / 20.0F) + yRotation)
-        cube.rotateZ((frameCount / 20.0F) + yRotation)
-        cube.box(cubeSize)
-        cube.endDraw()
-    }
+  fun drawCube(index: Int) {
+    cube.beginDraw()
+    cube.clear()
+    // cube.lights()
+    cube.stroke(230)
+    cube.strokeWeight(3F)
+    cube.noFill()
+    val row = index / numCubesInRow
+    val column = index % numCubesInRow
+    val widthIncrement = width / numCubesInRow.toFloat()
+    val heightIncrement = height / numCubesInRow.toFloat()
+    cube.translate(column * widthIncrement + cubeSize, row * heightIncrement + cubeSize)
+    val maxRotation = PI * 2
+    val xRotation = (maxRotation / numCubesInRow) * (row - numCubesInRow / 2) - PI
+    val yRotation = (maxRotation / numCubesInRow) * (column - numCubesInRow / 2) - PI
+    cube.rotateX((frameCount / 20.0F) + xRotation)
+    //cube.rotateY((frameCount / 20.0F) + yRotation)
+    cube.rotateZ((frameCount / 20.0F) + yRotation)
+    cube.box(cubeSize)
+    cube.endDraw()
+  }
 }
 
 fun main() {
-    PApplet.main("CubeGrid")
+  PApplet.main("CubeGrid")
 }
