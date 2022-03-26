c2a2458818c3f028d37612ab52395b5a93e2bbb3
Connor Hasting <guardianx515@gmail.com>
2022-03-26 15:43:22 -0500

    Improved performance of Schotter

--- src/main/kotlin/com/codedchai/sketch/animation/Schotter.kt
+++ src/main/kotlin/com/codedchai/sketch/animation/Schotter.kt
@@ -5,38 +5,44 @@ import com.codedchai.domain.schotter.SchotterOffsets
 import com.codedchai.domain.schotter.SchotterStone
 import com.codedchai.extensions.drawSchotterStone
 import com.codedchai.sketch.BaseSketch
-import kotlinx.coroutines.joinAll
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import processing.core.PApplet
 import kotlin.random.Random
+import kotlin.system.measureNanoTime
 
 class Schotter : BaseSketch() {
 
   private val MARGIN_FROM_SIDE = 110f
-  private val MARGIN_FROM_TOP = 100f
-  private val PERCENT_OF_STONE_MOTION = .4f
+  private val MARGIN_FROM_TOP = 10f
+  private val PERCENT_OF_STONE_MOTION = .3f
   private val MARGIN_BETWEEN_STONES = 20f
   private val HALF_MARGIN_BETWEEN_STONES = MARGIN_BETWEEN_STONES / 2f
-  private val NUM_ROWS = 12
+  private val NUM_ROWS = 11
 
-  private val schotterGrid: List<SchotterStone> = generateSchotterGrid(8, NUM_ROWS, 100f, MARGIN_BETWEEN_STONES)
+  private val schotterGrid: Sequence<SchotterStone> = generateSchotterGrid(8, NUM_ROWS, 100f, MARGIN_BETWEEN_STONES)
 
   override fun draw() {
-    pGraphics.beginDraw()
-    background(66)
-    pGraphics.translate(MARGIN_FROM_SIDE, MARGIN_FROM_TOP)
-    schotterGrid.forEach { pGraphics.drawSchotterStone(it) }
-    pGraphics.endDraw()
+    val renderTime = measureNanoTime {
+      pGraphics.beginDraw()
+      background(66)
+      pGraphics.translate(MARGIN_FROM_SIDE, MARGIN_FROM_TOP)
 
-    image(pGraphics, 0f, 0f)
+      schotterGrid.forEach { stone -> pGraphics.drawSchotterStone(stone) }
 
+      pGraphics.endDraw()
+
+      image(pGraphics, 0f, 0f)
+    }
     super.draw()
+    val updateTime = measureNanoTime {
+      updateSchotterGrid()
+    }
 
-    updateSchotterGrid()
+    println("Render time: $renderTime - Update time: $updateTime")
+
+    println(frameRate)
   }
 
-  fun generateSchotterGrid(numColumns: Int, numRows: Int, sideLength: Float, margin: Float): List<SchotterStone> {
+  fun generateSchotterGrid(numColumns: Int, numRows: Int, sideLength: Float, margin: Float): Sequence<SchotterStone> {
     val halfMargin = margin / 2f
     return (0 until numColumns).flatMap { x ->
       (0 until numRows).map { y ->
@@ -53,7 +59,7 @@ class Schotter : BaseSketch() {
           currentColNumber = x
         )
       }
-    }
+    }.asSequence()
   }
 
   fun getSchotterOffsets(factor: Float, halfMargin: Float): SchotterOffsets {
@@ -64,46 +70,45 @@ class Schotter : BaseSketch() {
     )
   }
 
-  fun updateSchotterGrid() = runBlocking {
-    schotterGrid.map { stone -> launch { updateSchotterStone(stone) } }.joinAll()
+  fun updateSchotterGrid() {
+    schotterGrid.forEach { stone -> updateSchotterStone(stone) }
   }
 
   fun updateSchotterStone(stone: SchotterStone) {
-    if (stone.cyclesUntilStationary > 0) {
-      stone.cyclesUntilStationary -= 1
-      stone.xTranslate += stone.xVelocity
-      stone.yTranslate += stone.yVelocity
-      stone.rotation += stone.rotationVelocity
-    } else {
-      val newCyclesUntilStationary = Random.nextInt(40, 230)
-      if (Random.nextFloat() < PERCENT_OF_STONE_MOTION) {
-        val factor = stone.currentRowNumber.toFloat() / NUM_ROWS.toFloat();
-        val (newXOffset, newYOffset, newRotation) = getSchotterOffsets(factor, MARGIN_BETWEEN_STONES)
-        val newXPosition =
-          stone.currentColNumber * stone.sideLength + MARGIN_BETWEEN_STONES * stone.currentColNumber + newXOffset
-        val newYPosition =
-          stone.currentRowNumber * stone.sideLength + MARGIN_BETWEEN_STONES * stone.currentRowNumber + newYOffset
-        stone.xVelocity = (newXPosition - stone.xTranslate) / newCyclesUntilStationary.toFloat()
-        stone.yVelocity = (newYPosition - stone.yTranslate) / newCyclesUntilStationary.toFloat()
-        stone.rotationVelocity = (newRotation - stone.rotation) / newCyclesUntilStationary.toFloat()
+    stone.apply {
+      if (cyclesUntilStationary > 0) {
+        cyclesUntilStationary -= 1
+        xTranslate += xVelocity
+        yTranslate += yVelocity
+        rotation += rotationVelocity
       } else {
-        stone.xVelocity = 0f
-        stone.yVelocity = 0f
-        stone.rotationVelocity = 0f
+        val newCyclesUntilStationary = Random.nextInt(40, 230)
+        if (Random.nextFloat() < PERCENT_OF_STONE_MOTION) {
+          val factor = currentRowNumber.toFloat() / NUM_ROWS.toFloat();
+          val (newXOffset, newYOffset, newRotation) = getSchotterOffsets(factor, MARGIN_BETWEEN_STONES)
+          val newXPosition = currentColNumber * sideLength + MARGIN_BETWEEN_STONES * currentColNumber + newXOffset
+          val newYPosition = currentRowNumber * sideLength + MARGIN_BETWEEN_STONES * currentRowNumber + newYOffset
+          xVelocity = (newXPosition - xTranslate) / newCyclesUntilStationary.toFloat()
+          yVelocity = (newYPosition - yTranslate) / newCyclesUntilStationary.toFloat()
+          rotationVelocity = (newRotation - rotation) / newCyclesUntilStationary.toFloat()
+        } else {
+          xVelocity = 0f
+          yVelocity = 0f
+          rotationVelocity = 0f
+        }
+        cyclesUntilStationary = newCyclesUntilStationary
       }
-      stone.cyclesUntilStationary = newCyclesUntilStationary
     }
   }
 
   override fun setup() {
     super.setup()
     pGraphics = createGraphics(width, height, P2D)
-    shouldSaveAnimation = true
     background(66)
   }
 
   override fun settings() {
-    size(1200, 1600, P2D)
+    size(1200, 1400, P2D)
   }
 }
 
